ba4f0d77b26f87d0d7d596f667346548f5ca56ea
Katherine Hambley <katherine.hambley@bcu.org>
2020-01-24 15:03:48 -0600

    Added initial project files

---
+++ ClientApp/src/app/registration/registration.component.ts
@@ -0,0 +1,16 @@
+import { Component, ChangeDetectorRef, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
